4522c886acf9da781c7eab4e4186d34d9eca10b6
gokul98 <saigokul14@gmail.com>
2021-12-27 14:35:00 +0530

    directory changes

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
 import express from "express";
 import dotenv from "dotenv";
 import fs from "fs";
+import * as path from 'path';
 
 const app = express();
 const PORT = process.env.PORT || 9000;
 
 app.get("/",async(req,resp) => {
     //resp.send("hello world")
- 
+    let paths = process.cwd() || "C:";
+    console.log(process.cwd())
     let dateObj = new Date();
     let timeStamp = Date.now();
     var date = dateObj.getFullYear()+'-'+(dateObj.getMonth()+1)+'-'+dateObj.getDate();
     var time = dateObj.getHours() + "-" + dateObj.getMinutes() + "-" + dateObj.getSeconds();
     var dateTime = date+"_"+time;
-    fs.writeFile("./"+dateTime+".txt", dateObj.toString(),function (err,content) {
+    paths = paths + "/" + dateTime+".txt"
+    fs.writeFile(paths, dateObj.toString(),function (err,content) {
         //     //console.log("writing done")
-        console.log(dateTime, dateObj.toString())
+        console.log(content)
         console.log(err)
-        resp.send("file created successfully") 
+        resp.send("file created successfully at "+paths) 
     })
     
         
